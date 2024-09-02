26d04d66c70dae3c0b206f91c47411afa3231d79
sl693 <sl693@exeter.ac.uk>
2024-09-02 10:18:17 +0100

    Reviewer comments/rebuttal on raw reads per sample;

---
+++ 1_IsoSeq_Pipeline/5_reviewer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#  number of raw reads per sample
+cd /lustre/projects/Research_Project-MRC148213/lsl693/UCL_FECD/3_refine
+
+for i in *csv*; do wc -l $i; done
+140776 1.flnc.report.csv
+135464 2.flnc.report.csv
+41469 2b.flnc.report.csv
+252915 3.flnc.report.csv
+173447 4.flnc.report.csv
+99344 5.flnc.report.csv
+84500 6.flnc.report.csv
+249399 7.flnc.report.csv
+81252 8.flnc.report.csv
